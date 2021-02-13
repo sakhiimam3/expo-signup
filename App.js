aafb72f294745b04119f19a98f15c94f9e6f1b63
sakhiimam3 <sakhiimam3411@gmail.com>
2021-02-13 23:24:58 +0500

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View ,TextInput,TouchableOpacity} from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.Heading}>SIGN UP PAGE</Text>
+      <TextInput style={styles.input}  placeholder={"name"} />
+      <TextInput style={styles.input} placeholder={"email"} />
+      <TextInput style={styles.input} placeholder={"password"} />
+      <TouchableOpacity   style={styles.btn}> 
+      <Text style={styles.btnText}>Submit</Text>
+      
+      </TouchableOpacity>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -16,6 +24,45 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop:100,
   },
+
+
+  Heading:{
+         color: "blue",
+         fontSize: 25,
+  },
+
+  input:{
+    height: 50,
+    width:180,
+     borderColor: 'gray', 
+     borderWidth: 1,
+     marginTop:20,
+     borderRadius:25,
+     paddingLeft:10,
+  },
+
+btn:{
+  height: 50,
+  width:180, 
+  marginTop:20,
+  backgroundColor:"green",
+  borderRadius:25,
+},
+btnText:{
+     
+      alignItems:'center',
+      justifyContent: 'center',
+      color:"white",
+      fontSize:20,
+      marginLeft:58,
+      marginTop:8,
+
+}
+
+
+
+
+
 });
